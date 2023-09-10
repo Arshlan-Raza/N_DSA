ffde071154dcfc222337b6155f2d822d2c6d7ac3
Arshlan <arshalanraja@gmail.com>
2023-09-10 17:16:55 +0530

    pattern printing

--- Patten/tempCodeRunnerFile.cpp
+++ Patten/tempCodeRunnerFile.cpp
@@ -1,1 +1,25 @@
-n - gaps + 1
+ for(int i = 0 ; i < n ; i++){
+
+    //     //inverted pyramid 1 
+    //     for(int j = 0 ; j < i ; j++){
+
+    //         cout<<"*";
+    //     }
+
+    //     //spaces 
+    //     for(int j = 0 ; j <2*n-i  ; j++){
+                
+    //         cout<<" ";
+
+    //     }
+           
+        
+    //     // inverted pyramid 2
+    //     for(int j = 0 ; j < i ;j++ ){
+
+    //         cout<<"*"; 
+
+    //     }
+
+    //     cout<<endl;
+    // }
