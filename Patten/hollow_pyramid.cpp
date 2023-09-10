a610b44e967176e0e4aecd7204f7464e8640e892
Arshlan <arshalanraja@gmail.com>
2023-09-10 17:16:34 +0530

    pattern printing

---
+++ Patten/hollow_pyramid.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n ;
+    cin>>n;
+
+    for(int i = 0 ; i < n ; i++){
+        
+        // spaces
+        for(int j = 0 ; j < n-i-1 ; j++){
+             
+             cout<<" ";
+   
+        }
+
+        //star
+        for(int j = 0 ; j < i+1 ; j++){
+
+            // print star for 1st and last column  
+            if(j  == 0 || j == i+1-1 ){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+
+        cout<<endl;
+    }
+}
