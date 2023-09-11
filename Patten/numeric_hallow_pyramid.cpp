e76969a3d27cb1c1fd37878cac8d1c8d51ec24a1
Arshlan <arshalanraja@gmail.com>
2023-09-11 07:20:08 +0530

    printing pattern 2nd day

---
+++ Patten/numeric_hallow_pyramid.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    int n  ; 
+    cin>>n;
+
+    for(int i = 0 ; i < n ; i++){
+
+        for(int j = 0 ; j < 2*i+1 ; j++){
+
+            if(i ==0 || i == n-1){
+                cout<<j+1; 
+            }
+            else{
+                if(j  == 0  ){
+                    cout<<"1";
+                }
+                else if(j == 2*i+1-1){
+                    cout<<i+1;
+                }
+                 else{
+                    cout<<" ";
+                 }
+            }
+           
+        }
+        cout<<endl;
+    }
+}
