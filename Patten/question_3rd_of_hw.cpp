86799d6089c1dcad1266fafbf6625bba003159fe
Arshlan <arshalanraja@gmail.com>
2023-09-14 22:28:22 +0530

    homework problem

---
+++ Patten/question_3rd_of_hw.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n ; 
+    cin>>n ;
+     
+    for(int i = 0 ; i < n ; i++){
+        
+        for(int j = 0 ; j < n + 3 - i ; j++){
+
+            cout<<"*";
+
+        }
+
+        for(int j = 0 ; j < 2 * i + 1 ; j++ ){
+
+            if(j % 2 != 0){
+                cout<<"*";
+            }
+            else{
+                cout<<i+1;
+            }
+        }
+
+        for(int j = 0 ; j < n + 3 - i ; j++){
+
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
