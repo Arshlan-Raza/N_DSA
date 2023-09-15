2dc08adf742bf562fc046d73c78d01878517bb2c
Arshlan <arshalanraja@gmail.com>
2023-09-15 17:27:19 +0530

    playing with function

---
+++ Function/add.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int add(int a , int b){
+
+    int result = a + b ; 
+    return result ;
+}
+
+int main() {
+
+    int num1,num2;
+    cout<<"Enter the value a and b :";
+    cin>>num1>>num2;
+
+    // int result = 
+    cout<<add(num1,num2);
+
+}
+
