eae0ec1922d5fe42f72653399360851b78c8d837
Arshlan <arshalanraja@gmail.com>
2023-09-15 17:21:48 +0530

    playing with function

---
+++ Function/first.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void message() {
+    
+    for(int i = 0 ; i < 5 ; i++){
+
+        cout<<"Waiting for my Mehram"<<endl;
+    }
+}
+int main () {
+
+    message();
+    return 0 ; 
+    
+}
+
