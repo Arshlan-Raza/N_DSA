fd4e7c7e5c98a3ae7a6b4ebed58b0c288de76849
Arshlan <arshalanraja@gmail.com>
2023-09-15 16:18:17 +0530

    playing with type casting

---
+++ typecasting/implicit_type_casting.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // example 1 implicit type casting 
+    // int num1 =10;
+    // float num2 = 5.5;
+
+    // float result = num1 + num2 ; 
+
+    // cout<<result;
+
+    // example 2  char to int 
+    // char ch = 'A';
+    // int a  = ch + 1 ;
+
+    // cout<<a<<endl;
+
+    // example 3 int to char  
+    int  a = 65; 
+    char ch = a;
+
+    cout<<ch;
+
+}
