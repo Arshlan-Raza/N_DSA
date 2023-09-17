3d6fb8a096e2a03a016a779dcf978288f93ae551
Arshlan <arshalanraja@gmail.com>
2023-09-17 14:33:29 +0530

    playing with function

---
+++ Week2_assignment/number_using_digits.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+
+int createNumber(int n) {
+
+        int num = 0 ;
+        
+        for(int i = 0 ; i < n ; i ++) {
+
+            cout<<"Enter the digits:";
+            cin>> 
+        }
+
+}
+
+int main() {
+
+    int numberOfDigits ;
+
+    int res = createNumber(numberOfDigits)
+
+    
+    
+}
