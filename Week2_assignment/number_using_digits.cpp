3cca4b069c0fb62ad31d82315802d4eb39f2a5e8
Arshlan <arshalanraja@gmail.com>
2023-09-20 22:00:37 +0530

    #

--- Week2_assignment/number_using_digits.cpp
+++ Week2_assignment/number_using_digits.cpp
@@ -7,7 +7,7 @@ int createNumber(int n) {
 
         int num = 0 ;
         
-        for(int i = 0 ; i < n ; i ++) {
+        for(int i = 0 ; i < n ; i ++) {   
 
             cout<<"Enter the digits:";
             cin>> 
