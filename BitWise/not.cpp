d292ab86d0b65c7b3a25c3b9a0c8fc09e007277e
Arshlan <arshalanraja@gmail.com>
2023-10-04 06:41:22 +0530

    #

--- BitWise/not.cpp
+++ BitWise/not.cpp
@@ -1,6 +1,8 @@
 // If My servants ask you about Me, well, I am near; I answer the prayer of the supplicant when he calls to Me.
 // Let them then respond to Me, and believe in Me, so that they may follow the right way.
 
+// My mercy encompasses all things
+
 #include<iostream>
 
 using namespace std;
