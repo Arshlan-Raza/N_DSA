1d3b1a6b9b732d1b48b430e72c2d417ad8909766
Arshlan <arshalanraja@gmail.com>
2023-12-07 16:38:41 +0530

    call by value

---
+++ Arrays/byvalue_byreference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int incrementBY1(int n){
+    n = n + 1;
+    return n ;
+}
+
+int main(){
+    int n ;
+    cin>>n;
+    n = incrementBY1(n);
+    cout<<n<<endl;
+    return 0;
+}
