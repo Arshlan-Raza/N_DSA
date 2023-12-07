4fa5fcd4bbebe4a49c5e47ff87b3296fb1cf0753
Arshlan <arshalanraja@gmail.com>
2023-12-07 15:18:49 +0530

    learning the concepts

---
+++ Arrays/code1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int arr[101];
+    char ch [101];
+    bool flags[101];
+
+    cout<<"My array created successful "<<endl;
+    // cout<<"Address of my arr:"<<addressof(arr)<<endl;
+
+    cout<<"Address of my arr :"<<arr<<endl;
+
+
+    cout<<"Size of my array of type integer is :"<<sizeof(arr)<<endl;
+
+
+}
