d221654cabd2c72666325506bf77bb42950e6dbc
Arshlan <arshalanraja@gmail.com>
2023-12-10 07:31:54 +0530

    codes of 2d array

--- Arrays/2d_array.cpp
+++ Arrays/2d_array.cpp
@@ -11,6 +11,16 @@ void printArray(int arr[][2], int row , int col){
     }
 }
 
+// column wise access 
+void colWiseAccesPrint(int arr[][2],int row, int col){
+    for(int i = 0 ; i < col ; i++){
+        for(int j = 0 ; j < row ; j++){
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int arr[2][2] = {
         {1,2},
@@ -20,6 +30,8 @@ int main(){
     // for intialiasing column is given mandatory 
     int arr2[4][4]; 
 
+    colWiseAccesPrint(arr,2,2);
+    cout<<endl;
     printArray(arr,2,2);
-    
+
 }
