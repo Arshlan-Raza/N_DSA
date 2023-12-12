7b39035c3e61d8b4778a82ded1d388544f5d3554
Arshlan <arshalanraja@gmail.com>
2023-12-12 07:07:53 +0530

    debugged the sorting 1's and 0's

---
+++ Debug-Exercise-C++ (Week-3)/sortZeroAndOne.cpp
@@ -0,0 +1,11 @@
+void sort0sand1s(int input[], int size){
+	int nextZero = 0;
+	for(int i=0; i<size; i++){
+		if(input[nextZero] == 0){
+			int temp = input[nextZero];
+			input[nextZero] = input[i];
+			input[i] = temp;
+            nextZero++;
+		}
+	}
+}
