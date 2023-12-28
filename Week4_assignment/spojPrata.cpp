73a2bb9e434b823c03eb25ad87b66650effdf19e
Arshlan <arshalanraja@gmail.com>
2023-12-28 18:19:54 +0530

    completed roti parata solution

---
+++ Week4_assignment/spojPrata.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool isPossibleSolution(vector<int>&cookRanks,int nP, int mid){
+    int currP = 0 ;
+    for(int i = 0 ; i < cookRanks.size();i++){
+        int R = cookRanks[i],j = 1;
+        int timeTaken = 0;
+        while(true){
+            if(timeTaken + j *R <= mid){
+                ++currP;
+                timeTaken += j*R;
+                ++j;
+            }else{
+                break;
+            }
+        }
+        if(currP >= nP){
+            return true;
+        }
+    }
+    return false;
+}
+
+int minTimeCompleteOrder(vector<int>&cookRanks,int nP){
+    int start = 0 ;
+    int highestRank = *max_element(cookRanks.begin(),cookRanks.end());
+    int end = highestRank * (nP * (nP + 1)/2);
+    int ans = -1;
+
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        if(isPossibleSolution(cookRanks,nP,mid)){
+            ans = mid ;
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    int T ;cin>>T;
+    while(T--){
+        int nP,nC;
+        cin>>nP>>nC;
+        vector<int> cookRanks;
+        while(nC--){
+            int R;cin>>R;
+            cookRanks.push_back(R);
+        }
+
+        cout<<minTimeCompleteOrder(cookRanks,nP)<<endl;
+
+    }
+   
+}
