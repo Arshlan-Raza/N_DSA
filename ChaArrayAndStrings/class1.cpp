1357c7e430946a36ef1efb2c814f09e32254b8d9
Arshlan <arshalanraja@gmail.com>
2024-01-02 07:29:53 +0530

    learn about char and strings

---
+++ ChaArrayAndStrings/class1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+    char ch[10];
+    cin.getline(ch,100) ;
+
+    cout<<"The value of character array is :"<<endl;
+
+    cout<<ch;
+
+}
