40ae3c0651ce5e55aa1d76763649f489de150f16
Arshlan <arshalanraja@gmail.com>
2024-01-04 05:59:22 +0530

    divide  with precision

--- searching_sorting/quotient.cpp
+++ searching_sorting/quotient.cpp
@@ -32,11 +32,29 @@ int getQuotient(int divisor , int dividend){
     return ans ;
 }
 
-int main(){
-    int divisor = 0;
-    int dividend = 625;
+double myPrecision(int divisor ,int dividend ){
+        double quotient = getQuotient(divisor,dividend);
 
-    int ans = getQuotient (abs(divisor),abs(dividend));
+        double step = 0.1;
+        int precision = 12;
+
+        for(int i = 0 ; i < precision ; i++){
+            double j = quotient ;
+            while(j * divisor <= dividend){
+                quotient = j ;
+                j += step ;
+            }
+            step = step/10;
+        }
+        
+        return quotient ;
+}
+
+int main(){
+    int divisor = 7;
+    int dividend = 22;
+
+    double ans = myPrecision (abs(divisor),abs(dividend));
     
     if((divisor > 0 && dividend < 0) || (dividend > 0 && divisor < 0)){
         ans = 0 - ans;
