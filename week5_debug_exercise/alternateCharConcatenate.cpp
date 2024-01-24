2fa1b8bbd63b14785d5743c26f20373e0d8c3cab
Arshlan <arshalanraja@gmail.com>
2024-01-24 08:04:46 +0530

    debugged the code

---
+++ week5_debug_exercise/alternateCharConcatenate.cpp
@@ -0,0 +1,39 @@
+
+/*
+Test Case:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+*/
+
+#include<iostream>
+
+using namespace std ;
+
+string mergeAlternately(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        string result = "";
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.push_back(word1[i++]);
+            }
+            if (j < n) {
+                result.push_back(word2[j++]);
+            }
+        }
+
+        return result;
+}
+
+int main() {
+    string word1 = "abc";
+    string word2 = "pqr";
+
+    string merged = mergeAlternately(word1, word2);
+
+    cout<<merged<<endl;
+
+    return 0;
+}
